e2a067ba803a0c5bbad05de0ee155a6d0d9ae1ce
Ben Sachs <32985159+BenSachsD@users.noreply.github.com>
2021-06-21 15:39:53 -0500

    Add files via upload

---
+++ src/main/java/com/example/entree/IngredientList.java
@@ -0,0 +1,15 @@
+package com.example.entree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class IngredientList extends AppCompatActivity{
+
+
+    public void ingredientReturnOnClick(View view) {
+        setContentView(R.layout.activity_main);
+    }
+}
